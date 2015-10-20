d412b9ed20cea3f23464b43c1abd8265c71a2dfe
tymadsen <ty.a.madsen@gmail.com>
2015-10-20 14:08:31 -0600

    deleting 427

--- ECEN_425/Lab 3/lab3final.s
+++ ECEN_425/Lab 3/lab3final.s
@@ -1109,6 +1109,9 @@ L_clib_83:
 	sub	sp, 16
 	jmp	L_clib_84
 
+resetISR:
+	call resetHandler ; Exit program
+tickISR:
 saveState:
 	push ax						; Save the state
 	push bx						;				|
@@ -1119,47 +1122,35 @@ saveState:
 	push bp						;				|
 	push es						;				|
 	push ds						; Done saving state
-	push word [sp+18]
-	ret
-resetISR:
-	call resetHandler ; Exit program
-tickISR:
-	; push ax						; Save the state
-	; push bx						;				|
-	; push cx						;				|
-	; push dx						;				|
-	; push si						;				|
-	; push di						;				|
-	; push bp						;				|
-	; push es						;				|
-	; push ds						; Done saving state
-	call saveState
+	; push word [sp+18]
+	; ret
+	; call saveState
 	sti 							; Re-enable interrupts after saving stack
 	call tickHandler  ; Call tick handler
 	cli 							; Disable interrupts
 	jmp restoreState
-	; mov	al, 0x20			; Load nonspecific EOI value (0x20) into register al
-	; out	0x20, al			; Write EOI to PIC (port 0x20)
-	; pop ds						; Restore previous state
-	; pop es						;				|
-	; pop bp						;				|
-	; pop di						;				|
-	; pop si						;				|
-	; pop dx						;				|
-	; pop cx						;				|
-	; pop bx						;				|
-	; pop ax						;	Done restoring prev. state
-	; iret 							; Restore execution and re-enable interrupts
+	mov	al, 0x20			; Load nonspecific EOI value (0x20) into register al
+	out	0x20, al			; Write EOI to PIC (port 0x20)
+	pop ds						; Restore previous state
+	pop es						;				|
+	pop bp						;				|
+	pop di						;				|
+	pop si						;				|
+	pop dx						;				|
+	pop cx						;				|
+	pop bx						;				|
+	pop ax						;	Done restoring prev. state
+	iret 							; Restore execution and re-enable interrupts
 keypressISR:
-	; push ax						; Save the state
-	; push bx						;				|
-	; push cx						;				|
-	; push dx						;				|
-	; push si						;				|
-	; push di						;				|
-	; push bp						;				|
-	; push es						;				|
-	; push ds						; Done saving state
+	push ax						; Save the state
+	push bx						;				|
+	push cx						;				|
+	push dx						;				|
+	push si						;				|
+	push di						;				|
+	push bp						;				|
+	push es						;				|
+	push ds						; Done saving state
 	call saveState
 	sti 							; Re-enable interrupts after saving stack
 	call keyHandler 	; Call keypress handler
--- ECEN_427/Lab3Workspace/hello_world_0/Debug/src/render.d
+++
@@ -1,8 +0,0 @@
-src/render.d src/render.o: ../src/render.c ../src/render.h \
-  ../src/globals.h ../src/bitMaps.h
-
-../src/render.h:
-
-../src/globals.h:
-
-../src/bitMaps.h:
